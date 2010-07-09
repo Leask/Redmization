195b8d920906b31f4de87f33adf131ccf30cf119
sunxinghua <sunxinghua008@gmail.com>
2010-07-09 14:28:06 +0800

    test

--- redmine/feedback.php
+++ redmine/feedback.php
@@ -12,10 +12,10 @@ $username=$_GET['username'];
 $sql="INSERT INTO `feedback`.`feedbackview` (`id`, `url`, `line`, `content`,`filename`,`username`) VALUES (NULL, '$str', '$line', '$content', '$file','$username')";
 $result=@mysql_query($sql);
 if ($result){
-
+   echo "评论提交成功";
 } else {
-
+   echo "数据有误";
 }
 
-echo "评论提交成功";
+
 ?>
