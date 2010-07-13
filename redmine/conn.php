b6b313111903a764432c46fdd245c0fce182da11
sunxinghua <sunxinghua008@gmail.com>
2010-07-13 13:42:31 +0800

    change con

--- redmine/conn.php
+++ redmine/conn.php
@@ -3,4 +3,5 @@ $conn=@mysql_connect("localhost", "root", "AnYuan") or die("mysql数据库没有
 @mysql_select_db("feedback",$conn) or die ("无法连接feeback数据库");
 mysql_query("set names utf8");
 $str=stristr(__FILE__, "?");
+$str=substr($str, 1);
 ?>
