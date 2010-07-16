22a80dd8d4bfc5349425e4320bc4348414a4a2b3
sunxinghua <sunxinghua008@gmail.com>
2010-07-16 16:34:56 +0800

    test return data

--- code-review-plugin/feedback.php
+++ code-review-plugin/feedback.php
@@ -6,7 +6,7 @@ $file=$_POST['primary'];
 $username=$_POST['username'];
 $str=$_POST['url'];
 $sql="INSERT INTO `feedback`.`feedbackview` (`id`, `url`, `line`, `content`,`filename`,`username`) VALUES (NULL, '$str', '$line', '$content', '$file','$username')";
-$result=@mysql_query($sql);
+$result=mysql_query($sql);
 if ($result==true) {
     echo "提交成功";
 }
