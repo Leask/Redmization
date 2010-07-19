f84dad75dbc4704527e15f9b9f77fd8a0cf0376a
sunxinghua <sunxinghua008@gmail.com>
2010-07-19 17:53:14 +0800

    test email

--- code-review-plugin/feedback.php
+++ code-review-plugin/feedback.php
@@ -1,10 +1,20 @@
 <?php
 require_once ("conn.php");
+require_once ("email.php");
 $line=$_POST['line'];
 $content=addslashes($_POST['content']);
 $file=$_POST['primary'];
 $username=$_POST['username'];
 $str=$_POST['url'];
+$email= new Email(array(
+                 'host' => 'smtp.gmail.com',
+                 'port' => '587', // Backup port: 465
+                 'auth' => true,
+                 'username' => 'anyuanproject@gmail.com',
+                 'password' => 'AnYuan.Project.for.Redmine',
+                 'timeout' => 3,
+          ));
+$email->send('"sunxinghua" <sunxinghua008@gmail.com>', "孙兴华用来测试的", "还是来测试的");
 $sql="INSERT INTO `feedback`.`feedbackview` (`id`, `url`, `line`, `content`,`filename`,`username`) VALUES (NULL, '$str', '$line', '$content', '$file','$username')";
 $result=mysql_query($sql);
 ?>
