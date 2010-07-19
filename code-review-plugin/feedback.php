1fe6232b3d752b78457e00bdf028c3ff512fa2e5
sunxinghua <sunxinghua008@gmail.com>
2010-07-19 19:14:07 +0800

    submit email class

--- code-review-plugin/feedback.php
+++ code-review-plugin/feedback.php
@@ -1,10 +1,13 @@
 <?php
 require_once ("conn.php");
+
 $line=$_POST['line'];
 $content=addslashes($_POST['content']);
 $file=$_POST['primary'];
 $username=$_POST['username'];
 $str=$_POST['url'];
+
+
 $sql="INSERT INTO `feedback`.`feedbackview` (`id`, `url`, `line`, `content`,`filename`,`username`) VALUES (NULL, '$str', '$line', '$content', '$file','$username')";
 $result=mysql_query($sql);
 ?>
