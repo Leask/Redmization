fa9db29ee873e1907558dc5a70bc298c807e1295
sunxinghua <sunxinghua008@gmail.com>
2010-07-19 20:07:35 +0800

    get title

--- code-review-plugin/js/js.js
+++ code-review-plugin/js/js.js
@@ -64,7 +64,7 @@ jQuery("document").ready(function(){
                  var kk=jQuery(".filename").text();
                 jQuery.post("/redmization/code-review-plugin/sendmail.php",{
                     line:add,
-                    primary:kk,
+                    primary:ab,
                     content:content,
                     username:username
                 },function(data){
@@ -102,7 +102,6 @@ jQuery("document").ready(function(){
         var file=jQuery(this).parent().parent().parent().children().eq(1).text();
         var primary=jQuery(this).parent().parent().parent().parent().attr("id");
         var ab=jQuery(this).parent().parent().parent().parent().children().find("thead").children().text();
-        alert (ab);
         file=file.replace(/(^\s*)/g,"");
         jQuery.each(total, function(m){
             if (jQuery(total).eq(m).text()==""){
