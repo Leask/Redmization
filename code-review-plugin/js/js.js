e0353121d81d2beee82120d1e1d1533ab98bda9f
sunxinghua <sunxinghua008@gmail.com>
2010-07-19 20:03:24 +0800

    get title

--- code-review-plugin/js/js.js
+++ code-review-plugin/js/js.js
@@ -101,6 +101,8 @@ jQuery("document").ready(function(){
         var add="";
         var file=jQuery(this).parent().parent().parent().children().eq(1).text();
         var primary=jQuery(this).parent().parent().parent().parent().attr("id");
+        var ab=jQuery(this).parent().parent().parent().parent().children(".filename").text();
+        alert (ab);
         file=file.replace(/(^\s*)/g,"");
         jQuery.each(total, function(m){
             if (jQuery(total).eq(m).text()==""){
