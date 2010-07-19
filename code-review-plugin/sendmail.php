20a1878287e42989220946a6629ebfd5d4f4c8d9
sunxinghua <sunxinghua008@gmail.com>
2010-07-19 19:23:38 +0800

    submit email class

--- code-review-plugin/sendmail.php
+++ code-review-plugin/sendmail.php
@@ -11,6 +11,6 @@ $email= new Email(array(
                  'username' => 'anyuanproject@gmail.com',
                  'password' => 'AnYuan.Project.for.Redmine'
           ),"anyuanproject@gmail.com");
-$email->send('"$username"."<sunxinghua008@gmail.com>"', "$filename文件下$line", "$content");
+$email->send('"$username"."<sunxinghua008@gmail.com>"', "111", "$content");
 echo "邮件发送成功";
 ?>
