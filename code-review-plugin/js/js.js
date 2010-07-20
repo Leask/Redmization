aae08c98bdcf16257935a7105e39a5d7f7afc4cf
sunxinghua <sunxinghua008@gmail.com>
2010-07-20 10:38:10 +0800

    send mail

--- code-review-plugin/js/js.js
+++ code-review-plugin/js/js.js
@@ -65,6 +65,7 @@ jQuery("document").ready(function(){
                   var url=location.pathname;
                  var search=location.search;
                   url+=search;
+                  url=""+"http://testing.aysaas.com";
                 jQuery.post("/redmization/code-review-plugin/sendmail.php",{
                     line:add,
                     primary:ab,
