4638bf6c2f21bdf87630359c83a2d59d728b72e0
sunxinghua <sunxinghua008@gmail.com>
2010-07-20 12:15:46 +0800

    test

--- code-review-plugin/js/js.js
+++ code-review-plugin/js/js.js
@@ -62,14 +62,14 @@ jQuery("document").ready(function(){
             } else {
 		         url=jQuery("#content").children("h2").text();
                  var kk=jQuery(".filename").text();
-                jQuery.post("/redmization/code-review-plugin/sendmail.php",{
+              /**  jQuery.post("/redmization/code-review-plugin/sendmail.php",{
                     line:add,
                     primary:ab,
                     content:content,
                     username:username
                 },function(data){
                     alert (data);
-                });
+                });*/
                 jQuery.post("/redmization/code-review-plugin/feedback.php",{
                     line:add,
                     primary:primary,
