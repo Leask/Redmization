85d34ef874ea5371705c129e03813407d1d8b988
sunxinghua <sunxinghua008@gmail.com>
2010-07-20 09:33:01 +0800

    send mail of all

--- code-review-plugin/feedback.php
+++ code-review-plugin/feedback.php
@@ -1,13 +1,10 @@
 <?php
 require_once ("conn.php");
-
 $line=$_POST['line'];
 $content=addslashes($_POST['content']);
 $file=$_POST['primary'];
 $username=$_POST['username'];
 $str=$_POST['url'];
-
-
 $sql="INSERT INTO `feedback`.`feedbackview` (`id`, `url`, `line`, `content`,`filename`,`username`) VALUES (NULL, '$str', '$line', '$content', '$file','$username')";
 $result=mysql_query($sql);
 ?>
--- code-review-plugin/sendmail.php
+++ code-review-plugin/sendmail.php
@@ -1,20 +1,22 @@
 <?php
 require_once ("email.php");
-$line=str_replace("", "0", $_POST['line']);
-$content=addslashes($_POST['content']);
-$filename=$_POST['primary'];
+//$line=str_replace("", "0", $_POST['line']);
+//$content=addslashes($_POST['content']);
+//$filename=$_POST['primary'];
 $recevier=array(
           array("sunxinghua008","sunxinghua008@gmail.com"),
           array("xjzc"=>"xjzc01@gmail.com"),
           array("leaskh"=>"leaskh@gmail.com"),
           array("lovelywcm"=>"lovelywcm@gmail.com")
     );
-$username=$_POST['username'];
+//$username=$_POST['username'];
 for ($i=0;$i<count($recevier);$i++) {
     if ($recevier[$i][0]!=$username){
-        $newrecevier[]=$recevier[$i][0];
+        $newrecevier[]=$recevier[$i][1];
     }
 }
+print_r($newrecevier);
+die();
 $email= new Email(array(
                  'host' => 'smtp.gmail.com',
                  'port' => '587', // Backup port: 465
