a32cd1b456dee69f6192bdf67c3dd8022898da38
sunxinghua <sunxinghua008@gmail.com>
2010-07-22 17:25:27 +0800

    add space

--- code-review-plugin/js/js.js
+++ code-review-plugin/js/js.js
@@ -48,7 +48,7 @@ jQuery("document").ready(function(){
                                     if (kk==true){
                                         jQuery("#k"+k).text("*"+content);
                                     } else {
-                                        jQuery("#k"+k).text(content);
+                                        jQuery("#k"+k).text("&nbsp;"+content);
                                     }
                                     kk=false;
                                 }
@@ -82,7 +82,7 @@ jQuery("document").ready(function(){
                                 var div=document.createElement("div");
                                 now.parent().find("td").find("td").eq(1).append(div);
                                 div.setAttribute("class", "display1");
-                                now.parent().find("td").find("td").eq(1).find("div:last").append(username+":"+" "+content);
+                                now.parent().find("td").find("td").eq(1).find("div:last").append("&nbsp;"+username+":"+" "+content);
                             } else {
                                 var div=document.createElement("div");
                                 now.parent().find("td").find("td").eq(1).append(div);
