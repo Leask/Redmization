80cbb8e51bb03f027348031652a508713f90cd1a
sunxinghua <sunxinghua008@gmail.com>
2010-07-22 10:47:40 +0800

    return js.js

--- code-review-plugin/js/js.js
+++ code-review-plugin/js/js.js
@@ -78,9 +78,6 @@ jQuery("document").ready(function(){
                         if (textStatus=="success"){
                             jQuery(".display").remove();
                             jQuery("#content").val("");
-                            var name1=ab+line;
-                            alert (name1);
-                            now.parent().children().append("<a name="+name1+ " "+"id="+name1+">");
                             if (now.parent().find("td").find("td").eq(1).children().is("div")==true){
                                 var div=document.createElement("div");
                                 now.parent().find("td").find("td").eq(1).append(div);
