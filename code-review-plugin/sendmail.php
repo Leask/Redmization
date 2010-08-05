9b342d5e08608c6a2988da37b6ee20ef9de8f25c
sunxinghua <sunxinghua008@gmail.com>
2010-08-05 11:37:36 +0800

    sendmail添加权限

--- code-review-plugin/sendmail.php
+++ code-review-plugin/sendmail.php
@@ -1,19 +1,18 @@
 <?php
-$username = $_POST['username'];
-if ($username != "") {
+$content = addslashes($_POST['content']);
+if ($content != "") {
     require_once ("email.php");
     $line = $_POST['line'];
     $line = preg_replace("/,(.*)/", "*,\\1", $line);
-    $content = addslashes($_POST['content']);
     $filename = $_POST['primary'];
     $url = $_POST['title'];
+	$username = $_POST['username'];
     $recevier = array(
         array("sunxinghua008", "sunxinghua008@gmail.com"),
         array("xjzc", "xjzc01@gmail.com"),
         array("leask", "leaskh@gmail.com"),
         array("lovelywcm", "lovelywcm@gmail.com")
     );
-
     for ($i = 0; $i < count($recevier); $i++) {
         if ($recevier[$i][0] != $username) {
             $newrecevier[] = $recevier[$i][1];
@@ -27,7 +26,6 @@ if ($username != "") {
                 'password' => 'AnYuan.Project.for.Redmine'
                     ), "anyuanproject@gmail.com");
     $email->send($newrecevier, "[SaaS]" . " " . $filename, '', "<html><body>" . "<br>" . $username . "：" . "<a href=" . $url . ">" . $content . "</a>" . "</body></html>");
-    echo "邮件发送成功";
 } else {
     echo '<script type="text/javascript">alert ("你无权访问此文件!!!");history.go(-1);</script>';
 }
