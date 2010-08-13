78d20bc7141b4196f7b1d04b4886d8162215634a
sunxinghua <sunxinghua008@gmail.com>
2010-08-13 13:22:19 +0800

    恢复大家的邮件发送功能

--- code-review-plugin/sendmail.php
+++ code-review-plugin/sendmail.php
@@ -8,13 +8,15 @@ if (isset ($_POST['content'])) {
 	$username = $_POST['username'];
 	$content = addslashes($_POST['content']);
     $recevier = array(
-        array("sunxinghua008", "sunxinghua008@gmail.com")
-     
+        array("sunxinghua008", "sunxinghua008@gmail.com"),
+        array("xjzc", "xjzc01@gmail.com"),
+        array("Leask", "leaskh@gmail.com"),
+        array("lovelywcm", "lovelywcm@gmail.com")
     );
     for ($i = 0; $i < count($recevier); $i++) {
-       
+        if ($recevier[$i][0] != $username) {
             $newrecevier[] = $recevier[$i][1];
-     
+        }
     }
     $email = new Email(array(
                 'host' => 'smtp.gmail.com',
