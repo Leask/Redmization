288924f12a9ee214652ddaefb73a019b0874e1f3
sunxinghua <sunxinghua008@gmail.com>
2010-08-13 11:08:02 +0800

    更改数据库密码

--- code-review-plugin/conn.php
+++ code-review-plugin/conn.php
@@ -1,5 +1,5 @@
 <?php
-$conn=@mysql_connect("localhost", "root", "") or die("mysql数据库没有打开");
+$conn=@mysql_connect("localhost", "root", "AnYuan") or die("mysql数据库没有打开");
 @mysql_select_db("feedback") or die ("无法连接feeback数据库");
 mysql_query("set names utf8");
 ?>
